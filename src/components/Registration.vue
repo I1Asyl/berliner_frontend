cb19d0f64c7986f90a9af3cfa82cb1ce373d14b3
I1Asyl <altayyerassyl@gmail.com>
2023-04-28 15:36:50 +0200

    This commit was automatically pushed from I1Asyl's device on 04/28/2023.

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -7,24 +7,24 @@ defineEmits(["login", "close"])
     <Popup @close="$emit('close')">
         <template #name> Registration</template>
         <template #content>
-                <form method="post" class="form-wrapper" action="">
+                <form method="post" class="form-wrapper" action="http://127.0.0.1:8080/signup">
                     <div class="form-group">
                         <label for="username">Your username</label>
-                        <input type="text" class="form-control" id="username" placeholder="Your username">
+                        <input type="text" class="form-control" name="username" value="hoh" placeholder="Your username">
                     </div>
                     <div class="form-group">
-                        <label for="first-name">Your username</label>
-                        <input type="text" class="form-control" id="first-name" placeholder="Your name">
+                        <label for="first-name">Your first name</label>
+                        <input type="text" class="form-control" name="first-name" placeholder="Your name">
                     </div>
 
                     <div class="form-group">
-                        <label for="last-name">Your username</label>
-                        <input type="text" class="form-control" id="last-name" placeholder="Your last name">
+                        <label for="last-name">Your last name</label>
+                        <input type="text" class="form-control" name="last-name" placeholder="Your last name">
                     </div>                    
 
                     <div class="form-group">
                         <label for="password">Your password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Your password">
+                        <input type="password" class="form-control" name="password" value="test" placeholder="Your password">
                     </div>
                     <button type="submit" class="btn btn-primary">Sign up</button>  
                     <button @click="$emit('login')" class="btn" type="button">Log in</button>
